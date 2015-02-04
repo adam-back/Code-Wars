586b5a614ffe98d1e25f83e41b49e30499e6fb7d
Adam Back <adb33@email.arizona.edu>
2015-02-04 12:14:38 -0800

    Wrote test cases.

--- test/morseTests.js
+++ test/morseTests.js
@@ -5,4 +5,36 @@ describe('Morse code', function() {
   it('should be a function', function() {
     expect(decodeMorse).to.be.a('function');
   });
+
+  it('should return a string', function() {
+    expect(decodeMorse('test')).to.be.a('string');
+  });
+
+  it('should translate a word', function() {
+    expect(decodeMorse('.- -.. .- --')).to.equal('ADAM');
+  });
+
+  it('should translate a sentence', function() {
+    expect(decodeMorse('.... . -.--   .--- ..- -.. .')).to.equal('HEY JUDE');
+  });
+
+  it('should remove spaces from before a word', function() {
+    expect(decodeMorse('   .- -.. .- --')).to.equal('ADAM');
+  });
+
+  it('should remove spaces from after a word', function() {
+    expect(decodeMorse('.- -.. .- --     ')).to.equal('ADAM');
+  });
+
+  it('should remove spaces from before and after a word', function() {
+    expect(decodeMorse('   .- -.. .- --  ')).to.equal('ADAM');
+  });
+
+  it('should recognize SOS alone', function() {
+    expect(decodeMorse('...---...')).to.equal('SOS');
+  });
+
+  it('should recognize SOS in a sentence', function() {
+    expect(decodeMorse('---- . .-.. .--.   ...---...   -.-.--')).to.equal('HELP SOS!');
+  });
 });
