b99c36a70c5762ff4e183456bff1573b6734f9da
Adam Back <adb33@email.arizona.edu>
2015-02-04 14:23:38 -0800

    Basic success.

--- morseCodeDecoder.js
+++ morseCodeDecoder.js
@@ -5,21 +5,38 @@ var decodeMorse = function(morseCode) {
   morseCode = morseCode.replace(/\s+$/, '');
 
   var message = '';
+  var words = [];
 
   // separate input by words (three spaces)
-  /(\s){3}/
+  // while( morseCode.search(/(\s){3}/) ) {
+  //   words.push( morseCode.substring(0, morseCode.search(/(\s){3}/)) );
+  //   // then remove the word
+  //   morseCode = morseCode.substring(morseCode.search(/(\s){3}/));
+  //   // remove space at the start
+  //   morseCode = morseCode.replace(/\s+/, '');
+  // }
+
+  // There should be one word left over
+  words.push(morseCode);
 
   // loop through words
+  for(var i = 0; i < words.length; i++) {
+    var word = words[i];
     // separate word into characters (one space)
-    var characters = morseCode.match(/(\S){1,}/g);
+    var characters = word.match(/(\S){1,}/g);
     
     // loop through characters
+    for (var i = 0; i < characters.length; i++) {
       // translate character to english, add to message
+      message += MORSE_CODE[characters[i]];
+    };
 
     // add space after word 
-
+    message += " ";
+  };
 
   // remove last space
+  message = message.substring(0, message.length - 1);
 
   // return english string
   return message;
