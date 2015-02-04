d5f675a169f75cb943fe60053acd6f9726ccb65e
Adam Back <adb33@email.arizona.edu>
2015-02-04 14:09:44 -0800

    Regexes written.

--- morseCodeDecoder.js
+++ morseCodeDecoder.js
@@ -1,12 +1,17 @@
 var decodeMorse = function(morseCode) {
   // remove white spaces at the start of the string
+  morseCode = morseCode.replace(/\s+/, '');
   // remove white space at the end of the string
+  morseCode = morseCode.replace(/\s+$/, '');
+
   var message = '';
 
   // separate input by words (three spaces)
+  /(\s){3}/
 
   // loop through words
     // separate word into characters (one space)
+    var characters = morseCode.match(/(\S){1,}/g);
     
     // loop through characters
       // translate character to english, add to message
