eb73ee03f55d01bad5e52230719ff91ec32ac36d
Adam Back <adb33@email.arizona.edu>
2015-02-11 17:21:41 -0800

    Wrote tests for callback closure problem.

---
+++ callbackClosure.js
@@ -0,0 +1,11 @@
+var createFunctions = function(n) {
+  var callbacks = [];
+
+  for (var i=0; i<n; i++) {
+    callbacks.push(function() {
+      return i;
+    });
+  }
+  
+  return callbacks;
+};
---
+++ test/callbackClosureTests.js
@@ -0,0 +1,42 @@
+describe.only('Callback closure', function() {
+  it('should be a function', function() {
+    expect(createFunctions).to.be.a('function');
+  });
+
+  it('should return an array', function() {
+    expect(Array.isArray(createFunctions(3))).to.be.true;
+  });
+
+  it('should have three functions', function() {
+    expect(createFunctions(3).length).to.equal(3);
+  });
+
+  it('should have five functions', function() {
+    expect(createFunctions(5).length).to.equal(5);
+  });
+
+  it('should have functions within the array', function() {
+    var callbacks = createFunctions(4);
+    
+    for (var i = 0; i < callbacks.length; i++) {
+      expect(callbacks[i]).to.be.a('function');
+    };
+  });
+
+  it('should return correct number', function() {
+    var callbacks = createFunctions(5);
+
+    for (var i=0; i<callbacks.length; i++) {
+      expect( callbacks[i]() ).to.equal(i);
+    }
+  });
+
+  it('should work for a larger (up to 50), random number of functions', function() {
+    var callbacks = createFunctions( Math.floor( Math.random() * 50 ) );
+    
+    for (var i=0; i<callbacks.length; i++) {
+      expect( callbacks[i]() ).to.equal(i);
+    }
+
+  });
+});
