0c6180c68574f88ce852e93c82243b3972ef5474
Adam Back <adb33@email.arizona.edu>
2015-03-14 11:45:01 -0700

    Passes all my tests.

--- camelToUnderscore.js
+++ camelToUnderscore.js
@@ -1,15 +1,25 @@
 var toUnderscore = function(camelCase) {
+  var output = [];
+
+  // norm the data into a string
+  camelCase = camelCase.toString();
   // change first letter to lower case string
   // push to output
+  output.push( camelCase[0].toLowerCase() );
 
   // loop through input
-    // if the item is alphabetic
-      // check if it's upper or lower case
-      // if it's uppercase
-        // push _ then put lowercase letter
-      // else lowercase
-        // push the letter
-    // not alphabetic
-      // push to output
+  for ( var i = 1; i < camelCase.length; i++ ) {
+    // check if it's upper or lower case
+    // if it's uppercase and NaN
+    if( camelCase[i].toUpperCase() === camelCase[i] && isNaN( camelCase[i] ) ) {
+      // push _ then put lowercase letter
+      output.push('_' + camelCase[i].toLowerCase() );
+    // else lowercase
+    } else {
+      output.push( camelCase[i] );
+    }
+
+  };
   // return underscored string
+  return output.join('');
 };
