133fe842ebc586220d64b4f24c185358a49b8fb8
Adam Back <adb33@email.arizona.edu>
2015-03-13 21:45:57 -0700

    Converting numbers up to 16 correctly.

--- rgbToHex.js
+++ rgbToHex.js
@@ -1,9 +1,10 @@
 var convertNumberToHex = function(num) {
   var hexDigits = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
   var current = ['0','0'];
-  var hexCounter = 0;
+  var hexCounter = 1;
   var placeValue = 1;
-  for (var i = 0; i <= num; i++) {
+  debugger;
+  for (var i = 1; i <= num; i++) {
     // if number is not at F
     if( hexCounter !== 16 ) {
       // replace it with the next highest
@@ -15,9 +16,13 @@ var convertNumberToHex = function(num) {
       // if it's not time to increase place values
       if( current[placeValue - 1] !== 'F' ) {
         hexCounter = 0;
+        // increase the value of the digit previous to the PV we're changing
         current[placeValue - 1] = hexDigits[ hexDigits.indexOf( current[placeValue - 1] ) + 1 ];
+        current.pop();
+        current.push( hexDigits[hexCounter] );
+        hexCounter++;
       } else {
-        
+        return 'FF';
       }
     }
   };
