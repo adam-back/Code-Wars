07bdbfd8e85b92aae95ffb824838b027900998e7
Adam Back <adb33@email.arizona.edu>
2015-03-14 11:35:40 -0700

    Pseudocode.

--- camelToUnderscore.js
+++ camelToUnderscore.js
@@ -1,3 +1,15 @@
 var toUnderscore = function(camelCase) {
-  
+  // change first letter to lower case string
+  // push to output
+
+  // loop through input
+    // if the item is alphabetic
+      // check if it's upper or lower case
+      // if it's uppercase
+        // push _ then put lowercase letter
+      // else lowercase
+        // push the letter
+    // not alphabetic
+      // push to output
+  // return underscored string
 };
