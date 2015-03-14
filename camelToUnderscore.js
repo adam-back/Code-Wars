bc04904ca4db8df794dc686c31552c8ce8f8c3c8
Adam Back <adb33@email.arizona.edu>
2015-03-14 11:27:34 -0700

    Added new problem.

---
+++ camelToUnderscore.js
@@ -0,0 +1,3 @@
+var toUnderscore = function(camelCase) {
+  
+};
---
+++ test/camelToUnderscoreTests.js
@@ -0,0 +1,5 @@
+describe('toUnderscore', function() {
+  it('should be a function', function() {
+    expect(toUnderscore('ab')).to.be.a('function');
+  });
+});
