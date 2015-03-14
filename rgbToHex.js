10f79d5d0115f61f2890010f4efccbea06ca3fd2
Adam Back <adb33@email.arizona.edu>
2015-03-13 22:00:01 -0700

    Cleanup.

--- rgbToHex.js
+++ rgbToHex.js
@@ -1,10 +1,10 @@
-var convertNumberToHex = function(num) {
+var convertNumberToHex = function( num ) {
   var hexDigits = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
   var current = ['0','0'];
   var hexCounter = 1;
   var placeValue = 1;
 
-  for (var i = 1; i <= num; i++) {
+  for ( var i = 1; i <= num; i++ ) {
     // if number is not at F
     if( hexCounter !== 16 ) {
       // replace it with the next highest
@@ -22,6 +22,7 @@ var convertNumberToHex = function(num) {
         current.push( hexDigits[hexCounter] );
         hexCounter++;
       } else {
+        // Highest colors go is FF
         return 'FF';
       }
     }
@@ -31,11 +32,10 @@ var convertNumberToHex = function(num) {
 };
 
 var rgb = function(n1, n2, n3) {
-  var numbers = [n1, n2, n3];
   var output = [];
 
-  for (var i = 0; i < numbers.length; i++) {
-    output.push( convertNumberToHex(numbers[i]) );
+  for (var i = 0; i < arguments.length; i++) {
+    output.push( convertNumberToHex(arguments[i]) );
   };
 
   return output.join('');
