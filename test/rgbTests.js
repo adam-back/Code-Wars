8d3163416c4ee5ca8f536fce9dc52e6516847414
Adam Back <adb33@email.arizona.edu>
2015-03-14 11:28:10 -0700

    Removed only from last problem's specs.

--- test/rgbTests.js
+++ test/rgbTests.js
@@ -1,4 +1,4 @@
-describe.only('Convert RGB to Hexadecimal', function() {
+describe('Convert RGB to Hexadecimal', function() {
   describe('Convert one number', function() {
     it('should be a function', function() {
       expect(convertNumberToHex).to.be.a('function');
