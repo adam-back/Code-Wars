47574158b78f7b211b6ebd5a1ba03f3e9a101a5f
Adam Back <adb33@email.arizona.edu>
2015-03-20 12:53:26 -0700

    Added test cases.

--- test/chainAddTests.js
+++ test/chainAddTests.js
@@ -1,5 +1,17 @@
-describe('Chaining add', function() {
+describe.only('Chaining add', function() {
   it('should be a function', function() {
     expect(add).to.be.a('function');
+  });
+
+  it('should add one number to eqaul 2', function() {
+    expect(add(2)).to.equal(2);
+  });
+
+  it('should add two numbers to equal 5', function() {
+    expect(add(2)(3)).to.equal(5);
+  });
+
+  it('should add three numbers to equal 8', function() {
+    expect(add(2)(1)(5)).to.equal(8);
   });
 });
