b1f3349194044679b5cf7c18ff7c6b024c5c01f9
Adam Back <adb33@email.arizona.edu>
2016-05-09 11:19:01 -0700

    (feat) Built basic set and get.

--- test/undoRedoTests.js
+++ test/undoRedoTests.js
@@ -1,10 +1,13 @@
-describe('undoRedo', function() {
-  var obj = {
-    x: 1,
-    y: 2
-  };
+describe.only('undoRedo', function() {
+  var obj, undidObj;
 
-  var undidObj = undoRedo( obj );
+  beforeEach(function() {
+    obj = {
+      x: 1,
+      y: 2
+    };
+    undidObj = undoRedo( obj );
+  });
 
   it('should be defined as a function', function() {
     expect( undoRedo ).to.be.a( 'function' );
@@ -19,12 +22,34 @@ describe('undoRedo', function() {
       expect( undidObj ).to.have.property( 'set' );
       expect( undidObj.set ).to.be.a( 'function' );
     });
+
+    it('should add a new key-value pair to the object', function() {
+      expect( undidObj ).not.to.have.a.property( 'Adam' );
+      undidObj.set( 'Adam', 1 );
+      expect( undidObj ).to.have.a.property( 'Adam' );
+      expect( undidObj.Adam ).to.equal( 1 );
+    });
+
+    it('should assign a new value to an existing key', function() {
+      expect( undidObj ).not.to.have.a.property( 'Adam' );
+      undidObj.set( 'Adam', 1 );
+      expect( undidObj ).to.have.a.property( 'Adam' );
+      expect( undidObj.Adam ).to.equal( 1 );
+      undidObj.set( 'Adam', 2 );
+      expect( undidObj.Adam ).to.equal( 2 );
+    });
   });
 
   describe('get', function() {
     it('should have a get method', function() {
       expect( undidObj ).to.have.property( 'get' );
       expect( undidObj.get ).to.be.a( 'function' );
+    });
+
+    it('should return value of given key', function() {
+      expect( undidObj.get( 'Adam' ) ).to.equal( undefined );
+      undidObj.set( 'Adam', 1 );
+      expect( undidObj.get( 'Adam' ) ).to.equal( 1 );
     });
   });
 
--- undoRedo.js
+++ undoRedo.js
@@ -1,17 +1,39 @@
 function undoRedo( object ) {
-  object.set = function( key, value ) {
+  // action, key
+  var _undoAction = undefined;
+  var _redoAction = undefined;
 
+  object.set = function( key, value ) {
+    this[ key ] = value;
+    _undoAction = [ 'del', key ];
+    _redoAction = undefined;
   };
 
   object.get = function( key ) {
-    
+    return this[ key ];
   };
 
-  object.del = function( key ) {};
+  object.del = function( key ) {
+    _undoAction = [ 'set', key ];
+    _redoAction = undefined;
+    return delete[ key ];
+  };
 
-  object.undo = function() {};
+  object.undo = function() {
+    if ( Array.isArray( _undoAction ) ) {
+      // something
+    } else {
+      throw new Error( 'There is nothing to undo.' );
+    }
+  };
 
-  object.redo = function() {};
+  object.redo = function() {
+    if ( Array.isArray( _redoAction ) ) {
+      _redoAction = _undoAction;
+    } else {
+      throw new Error( 'There is nothing to redo.' );
+    }
+  };
 
   return object;
-};
+}
