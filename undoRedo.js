855a9eec95545588d0d26fb439b25df8d52c2c42
Adam Back <adb33@email.arizona.edu>
2016-05-10 10:33:45 -0700

    (feat) Implemented undo and redo..

--- undoRedo.js
+++ undoRedo.js
@@ -14,14 +14,30 @@ function undoRedo( object ) {
   };
 
   object.del = function( key ) {
-    _undoAction = [ 'set', key ];
+    // set, key, value
+    _undoAction = [ 'set', key, this[ key ] ];
     _redoAction = undefined;
     return delete this[ key ];
   };
 
   object.undo = function() {
     if ( Array.isArray( _undoAction ) ) {
-      // something
+      var action= _undoAction[ 0 ];
+      var key = _undoAction[ 1 ];
+
+      if ( action === 'del' ) {
+        // set, key, value
+        _redoAction = [ 'set', key, this[ key ] ];
+        delete this[ key ];
+        _undoAction = undefined;
+        return;
+
+      } else {
+        _redoAction = [ 'del', key ];
+        this[ key ] = _undoAction[ 2 ];
+        _undoAction = undefined;
+        return;
+      }
     } else {
       throw new Error( 'There is nothing to undo.' );
     }
@@ -29,7 +45,22 @@ function undoRedo( object ) {
 
   object.redo = function() {
     if ( Array.isArray( _redoAction ) ) {
-      _redoAction = _undoAction;
+      var action= _redoAction[ 0 ];
+      var key = _redoAction[ 1 ];
+
+      if ( action === 'del' ) {
+        // set, key, value
+        _undoAction = [ 'set', key, this[ key ] ];
+        delete this[ key ];
+        _undoAction = undefined;
+        return;
+
+      } else {
+        _undoAction = [ 'del', key ];
+        this[ key ] = _redoAction[ 2 ];
+        _undoAction = undefined;
+        return;
+      }
     } else {
       throw new Error( 'There is nothing to redo.' );
     }
