72c29082bf304dd1b01e377a8837c1abf10e2934
Adam Back <adb33@email.arizona.edu>
2016-06-03 18:21:27 -0700

    (feat) Two solutions for bindConsole.

--- bindConsole.js
+++ bindConsole.js
@@ -1,8 +1,15 @@
-var bindConsole = function() {
-  for(var i = 0; i < 9; i++) {
-    var boundConsole = console.log.bind( null, i );
-    setTimeout(boundConsole, 0);
-  }
+// console's 0, 1, 2, 3, 4, 5, 6, 7, 8
 
-  // console's 0, 1, 2, 3, 4, 5, 6, 7, 8
+// solution 1
+for( var i = 0; i < 9; i++ ) {
+  setTimeout(function( data ) {
+    console.log( data );
+  }, 0, i );
+};
+
+// solution 2
+for( var i = 0; i < 9; i++ ) {
+  setTimeout(function() {
+    console.log( this );
+  }.bind( i ), 0 );
 };
