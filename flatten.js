032e57f246e7b54a3fbc2bd2fbef262248a709f2
Adam Back <adb33@email.arizona.edu>
2016-06-27 13:41:05 -0700

    (refactor) Combined RegEx search for brackets.

--- flatten.js
+++ flatten.js
@@ -1,8 +1,7 @@
 // Write some code, that will flatten an array of arbitrarily nested arrays of integers into a flat array of integers. e.g. [[1,2,[3]],4] -> [1,2,3,4].
 var flatten = function( nestedArray ) {
   nestedArray = JSON.stringify( nestedArray );
-  nestedArray = nestedArray.replace( /\[/g, '' );
-  nestedArray = nestedArray.replace( /\]/g, '' );
+  nestedArray = nestedArray.replace( /\[|\]/g, '' );
   nestedArray = ''.concat( '[', nestedArray, ']' );
   return JSON.parse( nestedArray );
 };
--- test/flattenTests.js
+++ test/flattenTests.js
@@ -6,4 +6,36 @@ describe('Flatten', function() {
   it('should return an array', function() {
     expect( Array.isArray( flatten( [] ) ) ).to.be.true;
   });
+
+  describe('should flatten array to [1,2,3,4]', function() {
+    it('for input [[1],2,3,4]', function() {
+      var input = [[1],2,3,4];
+      var output = flatten( input );
+      expect( output ).to.deep.equal( [ 1, 2, 3, 4 ] );
+    });
+
+    it('for input [1,[2,3],4]', function() {
+      var input = [1,[2,3],4];
+      var output = flatten( input );
+      expect( output ).to.deep.equal( [ 1, 2, 3, 4 ] );
+    });
+
+    it('for input [[1,2,[3]],4]', function() {
+      var input = [[1,2,[3]],4];
+      var output = flatten( input );
+      expect( output ).to.deep.equal( [ 1, 2, 3, 4 ] );
+    });
+
+    it('for input [[1,[2,3]],4]', function() {
+      var input = [[1,[2,3]],4];
+      var output = flatten( input );
+      expect( output ).to.deep.equal( [ 1, 2, 3, 4 ] );
+    });
+
+    it('for input [[1,2,3,4]]', function() {
+      var input = [[1,2,3,4]];
+      var output = flatten( input );
+      expect( output ).to.deep.equal( [ 1, 2, 3, 4 ] );
+    });
+  });
 });
