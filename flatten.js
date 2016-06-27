fe519160357b8a4af1321ded41b5beda4dd04910
Adam Back <adb33@email.arizona.edu>
2016-06-27 13:37:18 -0700

    (feat) Flattens arrays.

--- flatten.js
+++ flatten.js
@@ -1,6 +1,8 @@
-// Write some code, that will flatten an array of arbitrarily nested arrays of integers into a flat array of integers. e.g. [[1,2,[3]],4] -> [1,2,3,4]. 
-
+// Write some code, that will flatten an array of arbitrarily nested arrays of integers into a flat array of integers. e.g. [[1,2,[3]],4] -> [1,2,3,4].
 var flatten = function( nestedArray ) {
-  var flattenedArray = [];
-  return flattenedArray;
+  nestedArray = JSON.stringify( nestedArray );
+  nestedArray = nestedArray.replace( /\[/g, '' );
+  nestedArray = nestedArray.replace( /\]/g, '' );
+  nestedArray = ''.concat( '[', nestedArray, ']' );
+  return JSON.parse( nestedArray );
 };
