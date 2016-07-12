ec3e69d976f88a6059dd834b0bc50047e4c683ae
Adam Back <adb33@email.arizona.edu>
2016-07-11 21:58:43 -0700

    (feat) Converted binary to text.

--- base64Converter.js
+++ base64Converter.js
@@ -1,3 +1,17 @@
 var convertBase64 = function( message ) {
   return window.atob( message );
 };
+
+var convertBinaryToText = function( message ) {
+  var ascii_string = '';
+  var current_byte;
+  //split it into bytes
+  for ( var i = 0; i < message.length / 8; i++ ) {
+    //get the current byte
+    current_byte = message.substring( i * 8, ( i * 8 ) + 8 );
+    //get the charcode from the current_byte and add it to the ascii_string
+    ascii_string += String.fromCharCode( parseInt( current_byte, 2 ) );
+  }
+
+  return ascii_string;
+};
--- test/base64ConverterTests.js
+++ test/base64ConverterTests.js
@@ -1,16 +1,33 @@
 describe.only('Base 64 Converter', function() {
+  describe('convertBase64', function() {
+    var sixtyFour = 'MDExMDExMTEwMTExMDAwMDAxMTAwMTAxMDExMDExMTAw MDEwMDAwMDAxMTEwMDExMDExMDAxMDEwMTExMDAxMTAx MTAwMDAxMDExMDExMDEwMTEwMDEwMQ==';
 
-  var sixtyFour = 'MDExMDExMTEwMTExMDAwMDAxMTAwMTAxMDExMDExMTAw MDEwMDAwMDAxMTEwMDExMDExMDAxMDEwMTExMDAxMTAx MTAwMDAxMDExMDExMDEwMTEwMDEwMQ==';
+    it('should be defined as a function', function() {
+      expect( convertBase64 ).to.be.a( 'function' );
+    });
 
-  it('should be defined as a function', function() {
-    expect( convertBase64 ).to.be.a( 'function' );
+    it('should return a string', function() {
+      expect( convertBase64( sixtyFour ) ).to.be.a( 'string' );
+    });
+
+    it('should convert base64', function() {
+      expect( convertBase64( sixtyFour ) ).to.equal( '0110111101110000011001010110111000100000011100110110010101110011011000010110110101100101' );
+    });
   });
 
-  it('should return a string', function() {
-    expect( convertBase64( sixtyFour ) ).to.be.a( 'string' );
-  });
+  describe('convertBinaryToText', function() {
+    var binary = '0110111101110000011001010110111000100000011100110110010101110011011000010110110101100101';
 
-  it('should convert base64', function() {
-    expect( convertBase64( sixtyFour ) ).to.equal( '0110111101110000011001010110111000100000011100110110010101110011011000010110110101100101' );
+    it('should be defined as a function', function() {
+      expect( convertBinaryToText ).to.be.a( 'function' );
+    });
+
+    it('should return a string', function() {
+      expect( convertBinaryToText( binary ) ).to.be.a( 'string' );
+    });
+
+    it('should convert base64', function() {
+      expect( convertBinaryToText( binary ) ).to.equal( 'open sesame' );
+    });
   });
 });
