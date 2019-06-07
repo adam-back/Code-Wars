2762ea33e3b7fa74a244602930d4488d16858abf
Adam Jaffe Back <adb33@email.arizona.edu>
2019-06-07 15:52:54 -0700

    (refactor) Improve flatten with bettern docs and tests.

--- flatten.js
+++ flatten.js
@@ -1,7 +1,16 @@
-// Write some code, that will flatten an array of arbitrarily nested arrays of integers into a flat array of integers. e.g. [[1,2,[3]],4] -> [1,2,3,4].
-var flatten = function( nestedArray ) {
-  nestedArray = JSON.stringify( nestedArray );
-  nestedArray = nestedArray.replace( /\[|\]/g, '' );
-  nestedArray = ''.concat( '[', nestedArray, ']' );
-  return JSON.parse( nestedArray );
+/**
+ * Flatten an array of arbitrarily nested arrays of integers into a flat array
+ * of integers.
+ * @example
+ * flatten([[1,2,[3]],4]);
+ * // returns [1,2,3,4]
+ * @param {Array.<number[]>} [nestedArray=[]]
+ * @returns {Array.<number>}
+ */
+const flatten = (nestedArray=[]) => {
+  // 1. Stringify the input array
+  // 2. Replace the brackets with empty strings
+  // 3. Wrap the raw values with new brackets for single array
+  // 4. Parse the stringified array into a real array and return
+  return JSON.parse(`[${JSON.stringify(nestedArray).replace(/\[|\]/g, '')}]`);
 };
--- test/flattenTests.js
+++ test/flattenTests.js
@@ -1,41 +1,40 @@
-describe('Flatten', function() {
-  it('should be defined as a function', function() {
-    expect( flatten ).to.be.a( 'function' );
+describe('Flatten', () => {
+  it('should be defined as a function', () => {
+    expect(flatten).to.be.a('function');
   });
 
-  it('should return an array', function() {
-    expect( Array.isArray( flatten( [] ) ) ).to.be.true;
+  it('should return an array', () => {
+    expect(Array.isArray(flatten([]))).to.be.true;
   });
 
-  describe('should flatten array to [1,2,3,4]', function() {
-    it('for input [[1],2,3,4]', function() {
-      var input = [[1],2,3,4];
-      var output = flatten( input );
-      expect( output ).to.deep.equal( [ 1, 2, 3, 4 ] );
-    });
+  it('should return an empty array if no param is given', () => {
+    expect(Array.isArray(flatten())).to.be.true;
+    expect(flatten()).to.deep.equal([]);
+  });
 
-    it('for input [1,[2,3],4]', function() {
-      var input = [1,[2,3],4];
-      var output = flatten( input );
-      expect( output ).to.deep.equal( [ 1, 2, 3, 4 ] );
-    });
+  it('should not alter an already-flattened array', () => {
+    expect(flatten([1])).to.deep.equal([1]);
+  });
 
-    it('for input [[1,2,[3]],4]', function() {
-      var input = [[1,2,[3]],4];
-      var output = flatten( input );
-      expect( output ).to.deep.equal( [ 1, 2, 3, 4 ] );
-    });
+  it('should flatten the array to the answer for the ultimate question of life, the universe, and everything', () => {
+    expect(flatten([[4], [2]])).to.deep.equal([4, 2]);
+  });
 
-    it('for input [[1,[2,3]],4]', function() {
-      var input = [[1,[2,3]],4];
-      var output = flatten( input );
-      expect( output ).to.deep.equal( [ 1, 2, 3, 4 ] );
-    });
+  describe('should flatten arrays to [1,2,3,4]', () => {
+    const flattenedArray =  [1, 2, 3, 4];
+    const testCases = [
+      [[1],2,3,4],
+      [1,[2,3],4],
+      [[1,2,[3]],4],
+      [[1,2,3,4]],
+      [[1],[2],[[3]],[[[[4]]]]]
+    ];
 
-    it('for input [[1,2,3,4]]', function() {
-      var input = [[1,2,3,4]];
-      var output = flatten( input );
-      expect( output ).to.deep.equal( [ 1, 2, 3, 4 ] );
+    testCases.forEach(oneCase => {
+      it(`for input ${JSON.stringify(oneCase)}`, () => {
+        let output = flatten(oneCase);
+        expect(output).to.deep.equal(flattenedArray);
+      });
     });
   });
 });
